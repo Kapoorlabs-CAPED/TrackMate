3d05126e97a96f49508a1532f52298f204c0f47b
kapoorlab <randomaccessiblekapoor@gmail.com>
2022-05-03 13:24:40 +0200

    export action

---
+++ src/main/java/fiji/plugin/trackmate/action/OneatExporterAction.java
@@ -0,0 +1,57 @@
+package fiji.plugin.trackmate.action;
+
+import java.awt.Frame;
+
+import javax.swing.JOptionPane;
+
+import static fiji.plugin.trackmate.gui.Icons.TRACKMATE_ICON;
+import fiji.plugin.trackmate.Model;
+import fiji.plugin.trackmate.SelectionModel;
+import fiji.plugin.trackmate.Settings;
+import fiji.plugin.trackmate.TrackMate;
+import fiji.plugin.trackmate.gui.displaysettings.DisplaySettings;
+import fiji.plugin.trackmate.util.TMUtils;
+import net.imagej.ImgPlus;
+
+public class OneatExporterAction extends AbstractTMAction {
+
+	
+	public static final String INFO_TEXT = "<html>"
+			+ "This action initiates Oneat track correction for the tracking results. "
+			+ "<p> "
+			+ "Oneat is an action classification software in python "
+			+ "which provides csv files of event locations such as mitosis/apoptosis "
+			+ "using the csv file of event locations the tracks are corrected "
+			+ "and a new trackscheme is generated with corrected tracks. "
+			+ "</html>";
+
+	public static final String KEY = "LAUNCH_ONEAT";
+
+	public static final String NAME = "Launch Oneat track corrector";
+	
+	
+	@Override
+	public void execute(TrackMate trackmate, SelectionModel selectionModel, DisplaySettings displaySettings,
+			Frame gui) {
+		
+		Settings settings = trackmate.getSettings();
+		Model model = trackmate.getModel();
+		final ImgPlus img = TMUtils.rawWraps( settings.imp );
+		
+		if (gui!=null)
+		{
+			
+			
+			final TrackCorrectorPanel panel = new TrackCorrectorPanel(settings, model);
+			final int userInput = JOptionPane.showConfirmDialog(gui, panel, "Launch Oneat track corrector", JOptionPane.OK_CANCEL_OPTION, JOptionPane.QUESTION_MESSAGE, TRACKMATE_ICON);
+			if ( userInput != JOptionPane.OK_OPTION )
+				return;
+			
+			
+		}
+		
+		
+		
+	}
+
+}
--- src/main/java/fiji/plugin/trackmate/action/TrackCorrectorPanel.java
+++ src/main/java/fiji/plugin/trackmate/action/TrackCorrectorPanel.java
@@ -36,8 +36,7 @@ import fiji.plugin.trackmate.gui.components.ConfigurationPanel;
 import fiji.plugin.trackmate.util.TMUtils;
 import ij.ImageJ;
 import ij.plugin.PlugIn;
-
-
+import static fiji.plugin.trackmate.detection.DetectorKeys.KEY_TARGET_CHANNEL;
 import static fiji.plugin.trackmate.action.OneatCorrectorFactory.DIVISION_FILE;
 import static fiji.plugin.trackmate.gui.Fonts.SMALL_FONT;
 import static fiji.plugin.trackmate.action.OneatCorrectorFactory.APOPTOSIS_FILE;
@@ -47,124 +46,128 @@ import static fiji.plugin.trackmate.action.OneatCorrectorFactory.KEY_SIZE_RATIO;
 import static fiji.plugin.trackmate.action.OneatCorrectorFactory.KEY_CREATE_LINKS;
 import static fiji.plugin.trackmate.action.OneatCorrectorFactory.KEY_BREAK_LINKS;
 
-public class TrackCorrectorPanel extends JPanel 
-{
+public class TrackCorrectorPanel extends JPanel {
 	private static final long serialVersionUID = 1L;
-    private static File oneatdivisionfile;
-    private static File oneatapoptosisfile;
+	private static File oneatdivisionfile;
+	private static File oneatapoptosisfile;
 
-    private JButton Loaddivisioncsvbutton;
-    private JButton Loadapoptosiscsvbutton;
-    private  JFormattedTextField MinTracklet;
-    private  JFormattedTextField TimeGap;
-    private  JFormattedTextField MotherDaughterSizeRatio;
-    
-    private JCheckBox CreateNewLinks;
-    private JCheckBox BreakCurrentLinks;
-	
-	public TrackCorrectorPanel( final Settings settings, final Model model )
-	{
-		
+	private JButton Loaddivisioncsvbutton;
+	private JButton Loadapoptosiscsvbutton;
+	private JFormattedTextField MinTracklet;
+	private JFormattedTextField DetectionChannel;
+	private JFormattedTextField TimeGap;
+	private JFormattedTextField MotherDaughterSizeRatio;
+
+	private JCheckBox CreateNewLinks;
+	private JCheckBox BreakCurrentLinks;
+
+	public TrackCorrectorPanel(final Settings settings, final Model model) {
+
 		final GridBagLayout gridBagLayout = new GridBagLayout();
 		gridBagLayout.columnWidths = new int[] { 144, 0, 32 };
 		gridBagLayout.rowHeights = new int[] { 0, 0, 84, 0, 27, 0, 0, 0, 0, 37, 23 };
 		gridBagLayout.columnWeights = new double[] { 0.0, 1.0, 0.0 };
 		gridBagLayout.rowWeights = new double[] { 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0 };
-		setLayout( gridBagLayout );
-		
-		final JLabel lblOneatModel = new JLabel( "Load Oneat division detections:" );
-		lblOneatModel.setFont( SMALL_FONT );
+		setLayout(gridBagLayout);
+
+		final JLabel lblOneatModel = new JLabel("Load Oneat division detections:");
+		lblOneatModel.setFont(SMALL_FONT);
 		final GridBagConstraints gbcLblOneatModel = new GridBagConstraints();
 		gbcLblOneatModel.anchor = GridBagConstraints.EAST;
-		gbcLblOneatModel.insets = new Insets( 0, 5, 5, 5 );
+		gbcLblOneatModel.insets = new Insets(0, 5, 5, 5);
 		gbcLblOneatModel.gridx = 0;
 		gbcLblOneatModel.gridy = 2;
-		add( lblOneatModel, gbcLblOneatModel );
-		
-		
+		add(lblOneatModel, gbcLblOneatModel);
+
 		Loaddivisioncsvbutton = new JButton("Load Oneat division detections From CSV");
- 
- Loaddivisioncsvbutton.setHorizontalTextPosition( SwingConstants.LEFT );
-		Loaddivisioncsvbutton.setFont( SMALL_FONT );
+
+		Loaddivisioncsvbutton.setHorizontalTextPosition(SwingConstants.LEFT);
+		Loaddivisioncsvbutton.setFont(SMALL_FONT);
 		final GridBagConstraints gbcLoadDivision = new GridBagConstraints();
 		gbcLoadDivision.anchor = GridBagConstraints.EAST;
 		gbcLoadDivision.gridwidth = 2;
-		gbcLoadDivision.insets = new Insets( 0, 5, 5, 5 );
+		gbcLoadDivision.insets = new Insets(0, 5, 5, 5);
 		gbcLoadDivision.gridx = 0;
 		gbcLoadDivision.gridy = 4;
-		add( Loaddivisioncsvbutton, gbcLoadDivision );
-		
-		
+		add(Loaddivisioncsvbutton, gbcLoadDivision);
+
 		Loadapoptosiscsvbutton = new JButton("Load Oneat apoptosis detections From CSV");
-		Loaddivisioncsvbutton.setHorizontalTextPosition( SwingConstants.LEFT );
-		Loaddivisioncsvbutton.setFont( SMALL_FONT );
+		Loaddivisioncsvbutton.setHorizontalTextPosition(SwingConstants.LEFT);
+		Loaddivisioncsvbutton.setFont(SMALL_FONT);
 		final GridBagConstraints gbcLoadApoptosis = new GridBagConstraints();
 		gbcLoadApoptosis.anchor = GridBagConstraints.EAST;
 		gbcLoadApoptosis.gridwidth = 2;
-		gbcLoadApoptosis.insets = new Insets( 0, 5, 5, 5 );
+		gbcLoadApoptosis.insets = new Insets(0, 5, 5, 5);
 		gbcLoadApoptosis.gridx = 0;
 		gbcLoadApoptosis.gridy = 6;
-		add( Loadapoptosiscsvbutton, gbcLoadApoptosis );		
-		
+		add(Loadapoptosiscsvbutton, gbcLoadApoptosis);
+
+		DetectionChannel = new JFormattedTextField(Integer.valueOf(2));
+		DetectionChannel.setFont(new Font("Arial", Font.PLAIN, 10));
+		DetectionChannel.setColumns(15);
+		final GridBagConstraints gbc_DetectionChannel = new GridBagConstraints();
+		gbc_DetectionChannel.gridwidth = 3;
+		gbc_DetectionChannel.insets = new Insets(0, 5, 5, 5);
+		gbc_DetectionChannel.fill = GridBagConstraints.BOTH;
+		gbc_DetectionChannel.gridx = 0;
+		gbc_DetectionChannel.gridy = 8;
+		add(DetectionChannel, gbc_DetectionChannel);
 		
 		MinTracklet = new JFormattedTextField(Integer.valueOf(2));
-		MinTracklet.setFont( new Font( "Arial", Font.PLAIN, 10 ) );
-		MinTracklet.setColumns( 15 );
+		MinTracklet.setFont(new Font("Arial", Font.PLAIN, 10));
+		MinTracklet.setColumns(15);
 		final GridBagConstraints gbc_MinTracklet = new GridBagConstraints();
 		gbc_MinTracklet.gridwidth = 3;
-		gbc_MinTracklet.insets = new Insets( 0, 5, 5, 5 );
+		gbc_MinTracklet.insets = new Insets(0, 5, 5, 5);
 		gbc_MinTracklet.fill = GridBagConstraints.BOTH;
 		gbc_MinTracklet.gridx = 0;
-		gbc_MinTracklet.gridy = 8;
-		add( MinTracklet, gbc_MinTracklet );
-		
-		
+		gbc_MinTracklet.gridy = 10;
+		add(MinTracklet, gbc_MinTracklet);
+
 		TimeGap = new JFormattedTextField(Integer.valueOf(2));
-		TimeGap.setFont( new Font( "Arial", Font.PLAIN, 10 ) );
-		TimeGap.setColumns( 15 );
+		TimeGap.setFont(new Font("Arial", Font.PLAIN, 10));
+		TimeGap.setColumns(15);
 		final GridBagConstraints gbcTimeGap = new GridBagConstraints();
 		gbcTimeGap.gridwidth = 3;
-		gbcTimeGap.insets = new Insets( 0, 5, 5, 5 );
+		gbcTimeGap.insets = new Insets(0, 5, 5, 5);
 		gbcTimeGap.fill = GridBagConstraints.BOTH;
 		gbcTimeGap.gridx = 0;
-		gbcTimeGap.gridy = 10;
-		add( MinTracklet, gbcTimeGap );
-		
+		gbcTimeGap.gridy = 12;
+		add(MinTracklet, gbcTimeGap);
+
 		MotherDaughterSizeRatio = new JFormattedTextField(Double.valueOf(4));
-		MotherDaughterSizeRatio.setFont( new Font( "Arial", Font.PLAIN, 10 ) );
-		MotherDaughterSizeRatio.setColumns( 15 );
+		MotherDaughterSizeRatio.setFont(new Font("Arial", Font.PLAIN, 10));
+		MotherDaughterSizeRatio.setColumns(15);
 		final GridBagConstraints gbc_MotherDaughterSizeRatio = new GridBagConstraints();
 		gbc_MotherDaughterSizeRatio.gridwidth = 3;
-		gbc_MotherDaughterSizeRatio.insets = new Insets( 0, 5, 5, 5 );
+		gbc_MotherDaughterSizeRatio.insets = new Insets(0, 5, 5, 5);
 		gbc_MotherDaughterSizeRatio.fill = GridBagConstraints.BOTH;
 		gbc_MotherDaughterSizeRatio.gridx = 0;
-		gbc_MotherDaughterSizeRatio.gridy = 12;
-		add( MotherDaughterSizeRatio, gbc_MotherDaughterSizeRatio );
-		
+		gbc_MotherDaughterSizeRatio.gridy = 14;
+		add(MotherDaughterSizeRatio, gbc_MotherDaughterSizeRatio);
+
 		CreateNewLinks = new JCheckBox(" Create new mitosis events ");
-		CreateNewLinks.setHorizontalTextPosition( SwingConstants.LEFT );
-		CreateNewLinks.setFont( SMALL_FONT );
+		CreateNewLinks.setHorizontalTextPosition(SwingConstants.LEFT);
+		CreateNewLinks.setFont(SMALL_FONT);
 		final GridBagConstraints gbcChckbxCreateNewLinks = new GridBagConstraints();
 		gbcChckbxCreateNewLinks.anchor = GridBagConstraints.EAST;
 		gbcChckbxCreateNewLinks.gridwidth = 2;
-		gbcChckbxCreateNewLinks.insets = new Insets( 0, 0, 5, 5 );
+		gbcChckbxCreateNewLinks.insets = new Insets(0, 0, 5, 5);
 		gbcChckbxCreateNewLinks.gridx = 0;
-		gbcChckbxCreateNewLinks.gridy = 14;
-		add( CreateNewLinks, gbcChckbxCreateNewLinks );
-		
-		
+		gbcChckbxCreateNewLinks.gridy = 16;
+		add(CreateNewLinks, gbcChckbxCreateNewLinks);
+
 		BreakCurrentLinks = new JCheckBox(" Break current mitosis events ");
-		BreakCurrentLinks.setHorizontalTextPosition( SwingConstants.LEFT );
-		BreakCurrentLinks.setFont( SMALL_FONT );
+		BreakCurrentLinks.setHorizontalTextPosition(SwingConstants.LEFT);
+		BreakCurrentLinks.setFont(SMALL_FONT);
 		final GridBagConstraints gbcChckbxBreakCurrentLinks = new GridBagConstraints();
 		gbcChckbxBreakCurrentLinks.anchor = GridBagConstraints.EAST;
 		gbcChckbxBreakCurrentLinks.gridwidth = 2;
-		gbcChckbxBreakCurrentLinks.insets = new Insets( 0, 0, 5, 5 );
+		gbcChckbxBreakCurrentLinks.insets = new Insets(0, 0, 5, 5);
 		gbcChckbxBreakCurrentLinks.gridx = 0;
-		gbcChckbxBreakCurrentLinks.gridy = 16;
-		add( CreateNewLinks, gbcChckbxBreakCurrentLinks );
-		
-	
+		gbcChckbxBreakCurrentLinks.gridy = 18;
+		add(CreateNewLinks, gbcChckbxBreakCurrentLinks);
+
 		Loaddivisioncsvbutton.addActionListener(new ActionListener() {
 
 			@Override
@@ -188,20 +191,19 @@ public class TrackCorrectorPanel extends JPanel
 						return null;
 					}
 				};
-				
+
 				csvfile.setCurrentDirectory(new File(settings.imp.getOriginalFileInfo().directory));
 				csvfile.setDialogTitle("Division Detection file");
 				csvfile.setFileSelectionMode(JFileChooser.FILES_ONLY);
 				csvfile.setFileFilter(csvfilter);
 
-				if (csvfile.showOpenDialog(getParent()) == JFileChooser.APPROVE_OPTION) 
+				if (csvfile.showOpenDialog(getParent()) == JFileChooser.APPROVE_OPTION)
 
 					oneatdivisionfile = new File(csvfile.getSelectedFile().getPath());
 			}
-		
-	
-	});
-		
+
+		});
+
 		Loadapoptosiscsvbutton.addActionListener(new ActionListener() {
 
 			@Override
@@ -225,52 +227,43 @@ public class TrackCorrectorPanel extends JPanel
 						return null;
 					}
 				};
-				
+
 				csvfile.setCurrentDirectory(new File(settings.imp.getOriginalFileInfo().directory));
 				csvfile.setDialogTitle("Apoptosis Detection file");
 				csvfile.setFileSelectionMode(JFileChooser.FILES_ONLY);
 				csvfile.setFileFilter(csvfilter);
 
-				if (csvfile.showOpenDialog(getParent()) == JFileChooser.APPROVE_OPTION) 
+				if (csvfile.showOpenDialog(getParent()) == JFileChooser.APPROVE_OPTION)
 
 					oneatapoptosisfile = new File(csvfile.getSelectedFile().getPath());
 			}
-		
-		
-	});
-		
+
+		});
+
 	}
 
-	
-	public void setSettings( final Map< String, Object > settings )
-	{
+	public void setSettings(final Map<String, Object> settings) {
 		MinTracklet.setValue(settings.get(KEY_TRACKLET_LENGTH));
+		DetectionChannel.setValue(settings.get(KEY_TARGET_CHANNEL));
 		TimeGap.setValue(settings.get(KEY_TIME_GAP));
 		MotherDaughterSizeRatio.setValue(settings.get(KEY_SIZE_RATIO));
-		CreateNewLinks.setSelected( ( boolean ) settings.get( KEY_CREATE_LINKS ) );
-		BreakCurrentLinks.setSelected( ( boolean ) settings.get( KEY_BREAK_LINKS ) );
+		CreateNewLinks.setSelected((boolean) settings.get(KEY_CREATE_LINKS));
+		BreakCurrentLinks.setSelected((boolean) settings.get(KEY_BREAK_LINKS));
 	}
 
-	
-	public Map< String, Object > getSettings()
-	{
-		final Map< String, Object > settings = new HashMap<>();
-		
-		settings.put( DIVISION_FILE, oneatdivisionfile );
-		settings.put( APOPTOSIS_FILE, oneatapoptosisfile );
+	public Map<String, Object> getSettings() {
+		final Map<String, Object> settings = new HashMap<>();
+
+		settings.put(DIVISION_FILE, oneatdivisionfile);
+		settings.put(APOPTOSIS_FILE, oneatapoptosisfile);
 		settings.put(KEY_TRACKLET_LENGTH, ((Number) MinTracklet.getValue()).doubleValue());
 		settings.put(KEY_TIME_GAP, ((Number) TimeGap.getValue()).doubleValue());
 		settings.put(KEY_SIZE_RATIO, ((Number) MotherDaughterSizeRatio.getValue()).doubleValue());
 		settings.put(KEY_BREAK_LINKS, BreakCurrentLinks.isSelected());
 		settings.put(KEY_CREATE_LINKS, CreateNewLinks.isSelected());
-		
+		settings.put(KEY_TARGET_CHANNEL, ((Number) DetectionChannel.getValue()).doubleValue());
+
 		return settings;
 	}
 
-
-
-
-	
-	
-	
 }
